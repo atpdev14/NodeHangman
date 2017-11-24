f703b83f5bd8848ef619958d330a1e8df23188a4
atpdev14 <tproctor4414@hotmail.com>
2017-11-24 14:00:53 -0800

    Making progress on game functionality.  Not printing display correctly though when joining display array.

--- main.js
+++ main.js
@@ -5,9 +5,8 @@
 	//and compares their values to see if the letter exists in the word
 
 // CURRENT STATE
-// -Selects an answer from the array and converts it to a display of underscores
-// -working on guess js to take user input and compare the user input to the answer to see if the value exists
-// -need to incorporate guess.js into main.js
+// -Convert display to an array to update values then back to a string to log it
+// -currently has throw error.  Can not find module.
 
 var fs = require("fs");
 var wordConstructor = require("./wordConstructor.js");
@@ -16,13 +15,6 @@ var answerHidden = "";
 var alreadyGuessed = [];
 var allAnswers = ["rudabega", "karaoke", "banana",	 "gameboy", "pumpkin", "pumpernickel", "juniper", "agonized", "repugnant", "besmirch", "deadpan"];
 var randy = Math.floor(Math.random() * allAnswers.length);
-
-// -Reduce number of Guesses
-// -push guess into an array of all Guesses
-// -print all Guesses
-// -check if user's guess exists in newAnswer.answer
-// -if it does exist, update the display value at that index
-
 
 // ===================================
 // 		GAME PLAY
@@ -37,7 +29,7 @@ function gamePlay(){
 // ===================================
 // 		USER GUESS- Recursive
 // ===================================
-function guess() {
+function guess(){
 	inq.prompt([
 		{
 			type: "input",
@@ -45,16 +37,17 @@ function guess() {
 			name: "userGuess"
 		}
 		]).then(function(response){
+			
 			compareGuess(response);
 
 			if(totalGuesses > 0){
 				gamePlay();
-			}else{
+			}else {
 				console.log("Game Over!");
 			}				
 
 		 });
-}
+};
 
 // ===================================
 // 		COMPARE FUNCTION
@@ -63,26 +56,27 @@ function compareGuess(response){
 	totalGuesses--;
 	console.log("Guesses Left: " + totalGuesses);
 	alreadyGuessed.push(response.userGuess);
-	console.log("Already Guessed: " + alreadyGuessed);
+	console.log("Already Guessed: " + alreadyGuessed); 
 
 	if(newAnswer.answer.indexOf(response.userGuess) > -1){
+		var displayArray = newAnswer.answerDisplay.split("");
 		for(i = 0; i < newAnswer.answer.length; i++){
-			// console.log("Answer I: " + newAnswer.answer[i]);
-			// console.log("Answer Display: " + i + " " +  newAnswer.answerDisplay[i]);
 			if(response.userGuess === newAnswer.answer[i]){
-				console.log(newAnswer.answerDisplay[i]);
-				newAnswer.answerDisplay[i] = response.userGuess;
-				console.log(newAnswer.answerDisplay[i]);
+				displayArray[i] = response.userGuess;
 				console.log(newAnswer.answerDisplay);
 			}
 		}
 	}else{
 		console.log("Nope!");
 	}
+	// console.log(displayArray); 					//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	newAnswer.answerDisplay = displayArray.join();
+	console.log(newAnswer.answerDisplay);
 };
 
 //Create new answer object from constructor
 var newAnswer = new wordConstructor(allAnswers[randy]);
+newAnswer.print();
 //Designate number of guesses
 var totalGuesses = newAnswer.answer.length + 3;
 console.log("Total Guesses: " + totalGuesses);
--- wordConstructor.js
+++ wordConstructor.js
@@ -3,12 +3,12 @@ var generateAnswer = function(answer){
 	this.answer = answer;
 	this.answer = this.answer.split("");
 
-	this.print = function(){}
+	this.print = function(){
 		 for(i = 0; i < this.answer.length; i++){
 		 	this.answerDisplay += "_ ";
 		 }
 	console.log(this.answer);
 	console.log(this.answerDisplay);
 	}
-
+};
 module.exports = generateAnswer;
